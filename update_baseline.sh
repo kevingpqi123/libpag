17f14b224d8156e8e482dd5b092de8d737e900b3
kevingpqi <kevingpqi@tencent.com>
2022-04-01 16:54:37 +0800

    test04

--- update_baseline.sh
+++ update_baseline.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-{
-  CACHE_VERSION_FILE=./test/baseline/.cache/version.json
-  if [ -f "$CACHE_VERSION_FILE" ]; then
-    HAS_DIFF=$(git diff --name-only main:test/baseline/version.json $CACHE_VERSION_FILE)
-    if [[ ${HAS_DIFF} == "" ]]; then
-      exit 0
-    fi
-  fi
+#{
+#  CACHE_VERSION_FILE=./test/baseline/.cache/version.json
+#  if [ -f "$CACHE_VERSION_FILE" ]; then
+#    HAS_DIFF=$(git diff --name-only main:test/baseline/version.json $CACHE_VERSION_FILE)
+#    if [[ ${HAS_DIFF} == "" ]]; then
+#      exit 0
+#    fi
+#  fi
   echo "~~~~~~~~~~~~~~~~~~~Update Baseline Start~~~~~~~~~~~~~~~~~~~~~"
 #  CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 #  STASH_LIST_BEFORE=$(git stash list)
@@ -55,4 +55,4 @@
 
   echo "~~~~~~~~~~~~~~~~~~~Update Baseline END~~~~~~~~~~~~~~~~~~~~~"
   exit
-}
+#}
